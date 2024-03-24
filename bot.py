b64833e7c47fc98845b2883fca08090b3d410136
malikkkkkk123 <160301095+malikkkkkk123@users.noreply.github.com>
2024-03-24 08:41:19 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,13 +17,7 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send(f'Hi! I am a bot {bot.user}!')
 
-@bot.command()
-async def hi(ctx):
-    await ctx.send(f'Hi! I am a bot {bot.user}!')
 
-@bot.command()
-async def hey(ctx):
-    await ctx.send(f'Hi! I am a bot {bot.user}!')
 
 @bot.command()
 async def heh(ctx, count_heh = 5):
@@ -82,5 +76,31 @@ async def duck(ctx):
     '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
     image_url = get_duck_image_url()
     await ctx.send(image_url)
+
+@bot.command()
+async def polusi(ctx):
+    await ctx.send(f'Pencemaran atau polusi adalah masuk atau dimasukkannya mahluk hidup, zat, energi dan/ atau komponen lain ke dalam air atau udara. Pencemaran juga bisa berarti berubahnya tatanan (komposisi) air atau udara oleh kegiatan manusia dan proses alam, sehingga kualitas air/ udara menjadi kurang atau tidak dapat berfungsi lagi sesuai dengan peruntukkannya. Pencemaran terhadap lingkungan dapat terjadi di mana saja dengan laju yang sangat cepat, dan beban pencemaran yang semakin berat akibat limbah industri dari berbagai bahan kimia termasuk logam berat.')
+
+@bot.command()
+async def dampak_polusi(ctx):
+    await ctx.send(f'menganggu keseimbangan lingkungan , punah nya flora fauna , kesuburan tanah berkurang , dll')
+
+@bot.command()
+async def cara_menangani_polusi(ctx):
+    await ctx.send('''Kurangi menggunakan kendaraan bermotor. 
+Matikan mesin mobil. 
+Jangan membakar sampah. 
+Menanam dan merawat pohon. 
+Kurangi penggunaan energi di rumah.''')
+
+@bot.command()
+async def cara_membuat_kerajinan_plastik(ctx):
+    await ctx.send('''Cara membuatnya: 
+1. Potong sepertiga bagian bawah botol. 
+2. Pada salah satu sisi, potong botol menjadi dua bentuk segitiga sehingga terlihat seperti telinga. 
+3. Cat botol dengan warna yang diinginkan. 
+4. Gambar wajah hewan pada botol, misalnya wajah kucing atau anjing. 
+5. Isi botol dengan tanah dan tanaman yang ingin ditanam.''')
+
 
 bot.run('token')
